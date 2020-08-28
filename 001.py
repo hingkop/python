0ad12a2f768d9cf1782a485ed6fb0cf2df3a7840
hingkop <qkrtjswlswkd@naver.com>
2020-08-28 23:46:16 +0900

    final

--- 001.py
+++ 001.py
@@ -1,1 +1,11 @@
 #반복문 활용해 0-n 합 구하기
+def sum_to_n(n):
+    i = 0
+    sum = 0
+    while ( i<= n):
+        sum = sum + i
+        i = i+1
+    print (sum)
+
+if __name__ == '__main__':
+    sum_to_n(100)
