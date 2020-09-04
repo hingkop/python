5fbd09e5c2990891b6e7262a87136130a005488f
hingkop <qkrtjswlswkd@naver.com>
2020-09-04 12:03:15 +0900

    final

--- 006.py
+++ 006.py
@@ -2,5 +2,11 @@
 def f(n):
     if n ==0:
         return 0
-    print (n%10.end='')
+    print (n%10, end='')
     f(n//10)
+
+    
+if __name__ == '__main__':
+    num_str = input("입력: ")
+    num = int(num_str)
+    f(num)
