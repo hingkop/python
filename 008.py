d112d031022b177bbe6dbf183c2678a9a3cb6945
hingkop <qkrtjswlswkd@naver.com>
2020-09-09 16:02:53 +0900

    final

--- 008.py
+++ 008.py
@@ -2,19 +2,19 @@
 def f(n):
     i = 2
     while i < n:
-        if n%i==0:
+        if n % i == 0:
             break
         i = i+1 
-        
     if i==n:
         print("소수") 
     else:
         print("합성수")
 
-    if __name__=="__main__":
-        f(19)
-        f(130)
-        f(37)
-        f(20)
-        f(21)
+f(19) 
+f(130)
+f(37)
+f(20)
+f(21)
+
+
 
